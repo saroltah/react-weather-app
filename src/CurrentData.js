d4575429fc4c8a395a3139edc8790a1adbb52fbf
saroltah <saroltah@users.noreply.github.com>
2023-02-08 20:49:09 +0100

    added api and made it function

--- src/CurrentData.js
+++ src/CurrentData.js
@@ -10,21 +10,22 @@ export default function CurrentData(props) {
             <ul>
               <li className="cityName">{props.cityName}</li>
               <li className="emoji">
-                ☀ <span className="degree">30°</span>
+                <img src={props.icon} alt="weather icon" />
+                <span className="degree">°{props.degree}</span>
               </li>
-              <li className="description">Description</li>
+              <li className="description">{props.description}</li>
             </ul>
           </div>
           <div className="col">
             <ul>
               <li className="data">
-                Rain: <span className="rainData">72mm</span>
+                Clouds: <span className="cloudData">{props.clouds}</span>%
               </li>
               <li className="data">
-                Wind: <span className="windData">30 km/h</span>
+                Wind: <span className="windData">{props.wind}</span>km/h
               </li>
               <li className="data">
-                Humidiy: <span className="humidData">35%</span>
+                Humidiy: <span className="humidData">{props.humidity}</span>%
               </li>
             </ul>
           </div>
--- src/SearchEngine.js
+++ src/SearchEngine.js
@@ -1,13 +1,38 @@
 import React, { useState } from "react";
 import "./SearchEngine.css";
 import CurrentData from "./CurrentData";
+import axios from "axios";
 
 export default function SearchEngine() {
   let [city, setCity] = useState("");
-  let [cityName, setCityName] = useState("Oslo");
+  let [cityName, setCityName] = useState("City");
+  let [clouds, setClouds] = useState("0");
+  let [degree, setDegree] = useState("0");
+  let [icon, setIcon] = (
+    <img src="http://openweathermap.org/img/wn/01d@2x.png" alt="weather icon" />
+  );
+  let [description, setDescription] = useState("");
+  let [wind, setWind] = useState("0");
+  let [humidity, setHumidity] = useState("0");
 
-  function handleSubmit(event) {
+  function sendCity(event) {
     event.preventDefault();
+    let apiKey = "1fa04c70c5487af6b7c48dd7dfcb0b3f";
+    let units = "metric";
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+
+    axios.get(url).then(DisplayCityData);
+
+    function DisplayCityData(response) {
+      let weatherimage = response.data.weather[0].icon;
+      setIcon(`http://openweathermap.org/img/wn/${weatherimage}@2x.png`);
+      setDegree(Math.round(response.data.main.temp));
+      setDescription(response.data.weather[0].description);
+      setHumidity(response.data.main.humidity);
+      setWind(Math.round(response.data.wind.speed));
+      setClouds(response.data.clouds.all);
+    }
+
     setCityName(city);
   }
 
@@ -21,7 +46,7 @@ export default function SearchEngine() {
       <div className="container">
         <div className="row">
           <div className="col">
-            <form onSubmit={handleSubmit}>
+            <form onSubmit={sendCity}>
               <input
                 type="search"
                 placeholder="Type a city"
@@ -38,7 +63,16 @@ export default function SearchEngine() {
           </div>
         </div>
       </div>
-      <CurrentData cityName={cityName} />;
+      <CurrentData
+        cityName={cityName}
+        clouds={clouds}
+        degree={degree}
+        icon={icon}
+        description={description}
+        wind={wind}
+        humidity={humidity}
+      />
+      ;
     </div>
   );
 }
